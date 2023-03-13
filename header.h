6dc3ca68199d919da99fe2c5192293be6f0d5633
Илья Прамский <pramskii.is@phystech.edu>
2023-03-13 17:06:53 +0300

    add header file

---
+++ header.h
@@ -0,0 +1,40 @@
+typedef unsigned char byte;
+typedef unsigned short word;
+typedef word address;
+
+#define MEMSIZE (64*1024)
+#define ADR_MIN 0
+#define ADR_MAX 65535
+
+enum lognum
+{
+	ERROR,
+	INFO,
+	TRACE,
+	DEBUG,
+	MORE_DEBUG
+};
+
+byte mem[MEMSIZE];
+
+int log_level = MORE_DEBUG;
+
+void b_write(address adr, byte val);
+
+byte b_read(address adr);
+
+void w_write(address adr, word val);
+
+word w_read(address adr);
+
+void load_data();
+
+void mem_dump(address adr, int size);
+
+void load_file(const char * filename);
+
+void log(int level, char* message,...);
+
+int set_log_level(int level);
+
+void introduction(int argc, char* argv[]);
--- main.c
+++ main.c
@@ -4,47 +4,7 @@
 #include <errno.h>
 #include <stdarg.h>
 #include <string.h>
-
-typedef unsigned char byte;
-typedef unsigned short word;
-typedef word address;
-
-#define MEMSIZE (64*1024)
-#define ADR_MIN 0
-#define ADR_MAX 65535
-
-enum lognum
-{
-	ERROR,
-	INFO,
-	TRACE,
-	DEBUG,
-	MORE_DEBUG
-};
-
-byte mem[MEMSIZE];
-
-int log_level = MORE_DEBUG;
-
-void b_write(address adr, byte val);
-
-byte b_read(address adr);
-
-void w_write(address adr, word val);
-
-word w_read(address adr);
-
-void load_data();
-
-void mem_dump(address adr, int size);
-
-void load_file(const char * filename);
-
-void log(int level, char* message,...);
-
-int set_log_level(int level);
-
-void introduction(int argc, char* argv[]);
+#include "header.h"
 
 void test_mem()
 {
@@ -139,7 +99,7 @@ void w_write(address adr, word val)
 
 word w_read(address adr)
 {
-	//assert(sizeof(adr) == 2  && adr >= ADR_MIN && adr  <=ADR_MAX && Некорректное значение адреса); 
+	//assert(sizeof(adr) == 2  && adr >= ADR_MIN && adr  <= ADR_MAX && Некорректное значение адреса); 
 	return (word)mem[adr+1]<<8 | (word)mem[adr];
 }
 
